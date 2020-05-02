734ad823a8a3dd0a1ed041fbb1d956d4cb8fbb8d
Septian139 <58352120+Septian139@users.noreply.github.com>
2020-05-02 20:43:08 +0700

    project

---
+++ HXCS/Program.cs
@@ -0,0 +1,45 @@
+﻿using HXCS.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HXCS
+{
+    class Program
+    {
+        enum asd
+        {
+            a = 1,
+            b = 2,
+            c = 3
+        }
+        static void Main(string[] args)
+        {
+            HXCS.Base.HXCS main = new HXCS.Base.HXCS
+                (
+                    typeof(Iops),
+                    typeof(SecondTable),
+                     typeof(Oji)
+                );
+            string str = main.InitTable();
+
+            asd u = (asd)2;
+            int c = (int)u;
+
+            SecondTable st = main.ExecuteSelect<SecondTable>();
+
+            Console.WriteLine();
+
+            Console.WriteLine(u.ToString());
+            Console.WriteLine(c);
+
+            Console.WriteLine(str);
+            Console.Read();
+        }
+
+        
+    }
+}
